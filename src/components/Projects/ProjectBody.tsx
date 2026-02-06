a2f4410daa1ab4767603f35c2dec394e7fa5af4d
Akpa Solomon <solomonakpas@gmail.com>
2026-02-06 01:47:46 +0100

    Update ProjectBody.tsx

--- src/components/Projects/ProjectBody.tsx
+++ src/components/Projects/ProjectBody.tsx
@@ -37,12 +37,12 @@ const ProjectBody = () => {
 
     return (
         <div className='py-16 px-4 sm:px-6 lg:px-8'>
-            <div className='max-w-[1440px] mx-auto space-y-20'>
+            <div className='max-w-360 mx-auto space-y-20'>
                 <div className="grid grid-cols-1 lg:grid-cols-2 gap-10">
                     {projects.map((project) => (
                         <div
                             key={project.id}
-                            className="group relative w-full h-[550px] overflow-hidden border hover:shadow-lg transition"
+                            className="group relative w-full h-137.5 overflow-hidden border hover:shadow-lg transition"
                         >
                             {/* Background Image */}
                             <div
@@ -51,15 +51,18 @@ const ProjectBody = () => {
                             />
 
                             {/* Gradient Overlay */}
-                            <div className="absolute inset-0 bg-gradient-to-t from-black to-transparent opacity-80" />
+                            <div className="absolute inset-0 bg-linear-to-t from-black to-transparent opacity-80" />
 
                             {/* Content */}
                             <div className="absolute bottom-16 left-10 z-10">
                                 <h2 className="text-3xl font-bold mb-4 text-white">
                                     {project.title}
                                 </h2>
-                                
-                                <button className="py-2 px-6 bg-primary hover:bg-primary rounded-md text-sm font-medium text-white" onClick={() => navigate(`/project/${project.slug}`, {state: project})}>
+
+                                <button className="py-2 px-6 bg-primary hover:bg-primary rounded-md text-sm font-medium text-white"
+                                    onClick={() => window.location.href = `/project/${project.slug}`}
+                                // onClick={() => navigate(`/project/${project.slug}`, { state: project })}
+                                >
                                     View Details
                                 </button>
                             </div>
