a5a57fc2f2362fde71037849924069802f949a45
Akpa Solomon <solomonakpas@gmail.com>
2026-02-06 01:37:06 +0100

    Update imageAssets.ts

--- src/assets/imageAssets.ts
+++ src/assets/imageAssets.ts
@@ -2,7 +2,7 @@ import no_data from './no_data.png';
 import img1 from './img1.jpg';
 import img2 from './img2.jpg';
 import img3 from './img3.webp';
-import logo from './logo.png'
+import logo from './Logo.png'
 import logo1 from './logo1.png'
 import logo2 from './logo2.png'
 import logo3 from './logo3.png'
